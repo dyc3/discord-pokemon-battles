2ef5c9b637b184ad180dd4656420374ccaaf8a95
Carson McManus <carson.mcmanus1@gmail.com>
2021-03-26 15:46:32 -0400

    add more functions to battleapi

--- battleapi.py
+++ battleapi.py
@@ -3,8 +3,17 @@ from pkmntypes import *
 import aiohttp
 import logging
 import jsonpickle
+from pkmntypes import *
+from turns import *
 
 BASE_URL = "http://api:4000"
+
+async def generate_pokemon() -> dict:
+	"""Get a randomly generated pokemon from the API.
+	"""
+	async with aiohttp.ClientSession() as session:
+		async with session.get(f"{BASE_URL}/pokedex/generate") as resp:
+			return await resp.json()
 
 async def create_battle(teams: list[Team]) -> dict:
 	"""
@@ -20,8 +29,21 @@ async def create_battle(teams: list[Team]) -> dict:
 	async with aiohttp.ClientSession() as session:
 		async with session.post(f"{BASE_URL}/battle/new", data=jsonpickle.encode({ "teams": teams }, unpicklable=False)) as resp:
 			logging.debug(f"battle created: {resp.status}")
-			result = await resp.json()
+			result = await resp.json(content_type=None)
 			return {
 				"bid": result["BattleId"],
 				"active_pokemon": result["ActivePokemon"],
 			}
+
+async def get_battle_context(battle_id: int, target: int) -> dict:
+	logging.debug(f"getting battle context: battle={battle_id} target={target}")
+	async with aiohttp.ClientSession() as session:
+		async with session.get(f"{BASE_URL}/battle/context?id={battle_id}&target={target}") as resp:
+			result = await resp.json()
+	return result
+
+async def submit_turn(battle_id: int, target: int, turn: Turn):
+	logging.debug(f"submitting turn: battle={battle_id} target={target} turn={type(turn)}")
+	async with aiohttp.ClientSession() as session:
+		async with session.post(f"http://api:4000/battle/act?id={battle_id}&target={target}", data=turn.toJSON()) as resp:
+			pass
