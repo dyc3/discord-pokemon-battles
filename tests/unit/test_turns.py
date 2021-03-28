c85d55aee143dbac8c43acd4c53cccad8022c4f9
Carson McManus <carson.mcmanus1@gmail.com>
2021-03-28 16:21:16 -0400

    rename test suite

--- tests/unit/test_turns.py
+++ tests/unit/test_turns.py
@@ -2,7 +2,7 @@ import unittest
 from turns import *
 import json
 
-class TestStringMethods(unittest.TestCase):
+class TestTurns(unittest.TestCase):
 	def test_turns_to_json(self):
 		turn = FightTurn(party=0, slot=0, move=0)
 		self.assertEqual(json.loads(turn.toJSON())["type"], 0)
