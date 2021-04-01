7711edc96a20f4d522626bf07c60e31476075805
Carson McManus <dyc3@users.noreply.github.com>
2021-03-31 21:52:22 -0400

    Update scripts/test.sh
    Co-authored-by: A-very-Cunning-ham <ave210knife@gmail.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+grep -v -e pydocstyle -e yapf -e pre-commit requirements-dev.txt | xargs pip install
 find tests/unit -type d | grep -v __pycache__ | xargs -n 1 python -m unittest discover
