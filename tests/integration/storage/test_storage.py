46be9b0d46bc38104b4a62358e3703c6acbff78c
Carson McManus <carson.mcmanus1@gmail.com>
2021-04-02 21:38:17 -0400

    add storage module backed by mongodb

---
+++ tests/integration/storage/test_storage.py
@@ -0,0 +1,38 @@
+import unittest
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from turns import *
+from pkmntypes import *
+from unittest.mock import patch
+import asyncio
+import storage
+
+
+class TestStorage(unittest.TestCase):
+
+	def setUp(self):
+		self.loop = asyncio.new_event_loop()
+		asyncio.set_event_loop(self.loop)
+		self.client = AsyncIOMotorClient('mongodb://db/brock_test', io_loop=self.loop)
+		storage._set_client(self.client)
+
+	def tearDown(self):
+
+		async def tearDown_async():
+			await self.client.drop_database("brock_test")
+
+		self.loop.run_until_complete(tearDown_async())
+		self.loop.close()
+
+	def test_pokemon_save(self):
+
+		async def go():
+			pkmn = Pokemon()
+			await pkmn.save()
+			self.assertIsNotNone(pkmn._id)
+
+		return self.loop.run_until_complete(go())
+
+
+if __name__ == "__main__":
+	unittest.main()
